d1e79d46302c555298e9fa6134c874878bf0eb6f
nzixuan <nzixuan@gmail.com>
2024-01-15 15:32:07 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = {
+        name: 'John',
+        skills: ['JavaScript', 'CSS', 'HTML']
+    };
+    console.log(member.name);
+    console.log(member.skills[0]);
+    console.log(member.skills[1]);
+    console.log(member.skills[2]);
+}
